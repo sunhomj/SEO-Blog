5249dd45d534c2279d26d1392cb2474bc8198443
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-01-28 23:24:40 +1030

    folder structure changed

---
+++ frontend/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "SEOBLOG",
+    API_DEVELOPMENT: "http://localhost:8000/api"
+  }
+};
---
+++ frontend/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossOrigin="anonymous"
+          ></link>
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
