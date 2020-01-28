4767551d1b4e573eee9d689c74bd336313ed5304
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-01-28 20:32:00 +1030

    auth create random user ID handller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,34 @@
+const User = require("../models/user");
+const shortId = require("shortid");
+
 exports.signup = (req, res) => {
-  const { name, email, password } = req.body;
-  res.json({
-    user: { name, email, password }
+  //   const { name, email, password } = req.body;
+  //   res.json({
+  //     user: { name, email, password }
+  //   });
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: "Email is already exist"
+      });
+    }
+
+    const { name, email, password } = req.body;
+    let username = shortId.generate();
+    let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+    let newUser = new User({ name, email, password, profile, username });
+    newUser.save((err, success) => {
+      if (err) {
+        return res.status(400).json({
+          error: err
+        });
+      }
+      res.json({
+        user: success
+      });
+      //   res.json({
+      //     message: "Signup Success! Please Signin."
+      //   });
+    });
   });
 };
