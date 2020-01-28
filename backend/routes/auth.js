01abbc49f509dd76630bae36536c762cf8d19c75
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-01-28 21:18:43 +1030

    C:/Program Files/Git/secret route added to check

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin } = require("../controllers/auth");
+const { signup, signin, signout, requireSignin } = require("../controllers/auth");
 
 //validators
 const { runValidation } = require("../validators/index");
@@ -9,5 +9,15 @@ const { userSignupValidator, userSigninValidator } = require("../validators/auth
 router.post("/signup", userSignupValidator, runValidation, signup);
 
 router.post("/signin", userSigninValidator, runValidation, signin);
+
+router.get("/signout", signout);
+
+//test
+
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "you have access to secret page"
+  });
+});
 
 module.exports = router;
