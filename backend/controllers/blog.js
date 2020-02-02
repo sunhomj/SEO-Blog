d85b48a2dca4093eb8d88264ed6889b30daf5e1c
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-02-02 22:06:45 +1030

    excerpt helper function added

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -9,30 +9,58 @@ const _ = require("lodash");
 // to send any mongoose error to client
 const { errorHandler } = require("../helpers/errorHandler");
 const fs = require("fs");
+const { smartTrim } = require("../helpers/blog");
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   console.log(form);
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
+    const { title, body, categories, tags } = fields;
     if (err) {
       return res.status(400).json({
         error: "Image could not upload"
       });
     }
 
-    const { title, body, categories, tags } = fields;
+    if (!title || !title.length) {
+      return res.status(400).json({
+        error: "title is required"
+      });
+    }
+    if (!body || body.length < 200) {
+      return res.status(400).json({
+        error: "Content is too short"
+      });
+    }
+    if (!categories || categories.length === 0) {
+      return res.status(400).json({
+        error: "At least one category is required"
+      });
+    }
+
+    if (!tags || tags.length === 0) {
+      return res.status(400).json({
+        error: "At least one tag is required"
+      });
+    }
+
     let blog = new Blog();
     blog.title = title;
     blog.body = body;
+    blog.excerpt = smartTrim(body, 320, " ", " ...");
     blog.slug = slugify(title).toLowerCase();
     blog.mtitle = `${title} | ${process.env.APP_NAME}`;
     // grap 160 charicters for description
     blog.mdesc = stripHtml(body.substring(0, 160));
     blog.postedBy = req.user._id;
 
+    //handle data of categories and tags
+
+    let arrayOfCategories = categories && categories.split(",");
+    let arrayOfTags = tags && tags.split(",");
+
     if (files.photo) {
-      console.log(files.photo);
       if (files.photo.size > 10000000) {
         return res.status(400).json({
           error: "Image should be less than 1mb in size"
@@ -48,7 +76,30 @@ exports.create = (req, res) => {
           error: errorHandler(err)
         });
       }
-      res.json(result);
+      //   res.json(result);
+      Blog.findByIdAndUpdate(
+        result._id,
+        { $push: { categories: arrayOfCategories } },
+        { new: true }
+      ).exec((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        } else {
+          Blog.findByIdAndUpdate(result._id, { $push: { tags: arrayOfTags } }, { new: true }).exec(
+            (err, result) => {
+              if (err) {
+                return res.status(400).json({
+                  error: errorHandler(err)
+                });
+              } else {
+                res.json(result);
+              }
+            }
+          );
+        }
+      });
     });
   });
 };
---
+++ backend/helpers/blog.js
@@ -0,0 +1,10 @@
+exports.smartTrim = (str, length, delim, appendix) => {
+  if (str.length <= length) return str;
+  let trimmedStr = str.substr(0, length + delim.length);
+
+  let lastDelimIndex = trimmedStr.lastIndexOf(delim);
+  if (lastDelimIndex >= 0) trimmedStr = trimmedStr.substr(0, lastDelimIndex);
+
+  if (trimmedStr) trimmedStr += appendix;
+  return trimmedStr;
+};
