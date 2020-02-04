6f656f560a2d72071ef0cf558adedbe4a1e906aa
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-02-05 03:11:23 +1030

    reactQuill config seperation

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "265dbb1690eb703013aa";
+/******/ 	var hotCurrentHash = "ddba6a9d24ed12f661d3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
