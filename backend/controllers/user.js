b2ef9483f45591770d0d77fdeb1b209495020ae9
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-02-14 03:00:51 +1030

    user update and photo added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,9 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
 const { errorHandler } = require("../helpers/errorHandler");
+const _ = require("lodash");
+const fs = require("fs");
+const formidable = require("formidable");
 
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
@@ -38,5 +41,52 @@ exports.publicProfile = (req, res) => {
           blogs: data
         });
       });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Photo could not be uploaded"
+      });
+    }
+    let user = req.profile;
+    user = _.extend(user, fields);
+
+    if (files.photo) {
+      if (files.photo.size > 100000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb"
+        });
+      }
+      user.photo.data = fs.readFileSync(files.photo.path);
+      user.photo.contentType = files.photo.type;
+      user.save((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        user.hashed_password = undefined;
+        res.json(user);
+      });
+    }
+  });
+};
+
+exports.photo = (req, res) => {
+  const username = req.params.username;
+  User.findOne({ username }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found"
+      });
+    }
+    if (user.photo.data) {
+      res.set("Content-Type", user.photo.contentType);
+      return res.send(user.photo.data);
+    }
   });
 };
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "576968934ca5612e2914";
+/******/ 	var hotCurrentHash = "57f7b19c7f40b081c854";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
