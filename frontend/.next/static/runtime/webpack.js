ea2aff097ef7bb134ae296d841b16b1a139f0e7d
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-02-14 15:51:26 +1030

    user profile page WIP

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0e2d28c14b3c58b9c500";
+/******/ 	var hotCurrentHash = "2dce8eddba6cc23eb62f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/user/update.js
+++ frontend/pages/user/update.js
@@ -1,38 +1,15 @@
 import Layout from "../../components/Layout";
 import Private from "../../components/auth/Private";
-import App from "../_app";
+import ProfileUpdate from "../../components/auth/ProfileUpdate";
 import Link from "next/link";
 
-const UserIndex = () => {
+const UserProfileUpdate = () => {
   return (
     <Layout>
       <Private>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-12 pt-5 pb-5">
-              <h2>User Dashboard</h2>
-            </div>
-            <div className="col-md-4">
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <Link href="/user/crud/blog">
-                    <a>Create Blog</a>
-                  </Link>
-                </li>
-                <li className="list-group-item">
-                  <Link href="/user/crud/blogs">
-                    <a>Update/Delete Blog</a>
-                  </Link>
-                </li>
-
-                <li className="list-group-item">
-                  <Link href="/user/update">
-                    <a>Update Profile</a>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-8">right</div>
+            <ProfileUpdate />
           </div>
         </div>
       </Private>
@@ -40,4 +17,4 @@ const UserIndex = () => {
   );
 };
 
-export default UserIndex;
+export default UserProfileUpdate;
