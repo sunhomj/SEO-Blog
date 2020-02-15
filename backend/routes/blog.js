1934132626eccdb2fe189e61cf7deafcc0f8283e
sunhomj <55496992+sunhomj@users.noreply.github.com>
2020-02-15 22:50:45 +1030

    user blog router create, remove, update  added

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -21,6 +21,12 @@ router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
 router.put("/blog/:slug", requireSignin, adminMiddleware, update);
 router.get("/blog/photo/:slug", getphoto);
 router.post("/blogs/related", listRelated);
-
 router.get("/blogs/search", listSearch);
+
+// auth user blog crud
+
+router.post("/user/blog", requireSignin, authMiddleware, create);
+router.delete("/user/blog/:slug", requireSignin, authMiddleware, remove);
+router.put("/user/blog/:slug", requireSignin, authMiddleware, update);
+
 module.exports = router;
