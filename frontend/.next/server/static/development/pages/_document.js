71511154606984266181688164539850d4f3ceb2
Sunny <sunhomj@users.noreply.github.com>
2020-02-16 11:53:19 +1030

    merge

--- frontend/.next/server/static/development/pages/_document.js
+++ frontend/.next/server/static/development/pages/_document.js
@@ -2604,7 +2604,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var next_document__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_document__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
 /* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_2__);
-var _jsxFileName = "C:\\Users\\Administrator\\Documents\\GitHub\\SEO-Blog\\frontend\\pages\\_document.js";
+var _jsxFileName = "/Users/Slothums/Documents/GitHub/SEO-Blog/frontend/pages/_document.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
 
